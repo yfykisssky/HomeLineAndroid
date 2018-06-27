337f4bdbf213e544a26353556fd218a35c794bff
yangfengyuan <yangfengyuan@bestpay.com.cn>
2018-06-27 18:24:25 +0800

    悬浮窗

--- app/src/main/java/com/lineclient/home/homelineclient/fragment/VideoFragment.java
+++ app/src/main/java/com/lineclient/home/homelineclient/fragment/VideoFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.lineclient.home.homelineclient.R;
+import com.lineclient.home.homelineclient.view.RockerView;
 
 /**
  * Created by yangfengyuan on 2017/7/25.
@@ -31,6 +32,74 @@ public class VideoFragment extends Fragment implements View.OnClickListener{
 
     private void initView() {
 
+        RockerView rockerView = view.findViewById(R.id.rockerview);
+        if (rockerView!= null) {
+            rockerView.setCallBackMode(RockerView.CallBackMode.CALL_BACK_MODE_STATE_CHANGE);
+            rockerView.setOnShakeListener(RockerView.DirectionMode.DIRECTION_8, new RockerView.OnShakeListener() {
+                @Override
+                public void onStart() {
+
+                }
+
+                @Override
+                public void direction(RockerView.Direction direction) {
+                   // mLogLeft.setText("摇动方向 : " + getDirection(direction));
+                }
+
+                @Override
+                public void onFinish() {
+                   // mLogLeft.setText(null);
+                }
+            });
+            rockerView.setOnAngleChangeListener(new RockerView.OnAngleChangeListener() {
+                @Override
+                public void onStart() {
+                    //mLogRight.setText(null);
+                }
+
+                @Override
+                public void angle(double angle) {
+                    //mLogRight.setText("摇动角度 : " + angle);
+                }
+
+                @Override
+                public void onFinish() {
+                   // mLogRight.setText(null);
+                }
+            });
+        }
+    }
+
+
+    private void handleDirection(RockerView.Direction direction) {
+        switch (direction) {
+            case DIRECTION_LEFT:
+
+                break;
+            case DIRECTION_RIGHT:
+
+                break;
+            case DIRECTION_UP:
+
+                break;
+            case DIRECTION_DOWN:
+
+                break;
+            case DIRECTION_UP_LEFT:
+
+                break;
+            case DIRECTION_UP_RIGHT:
+
+                break;
+            case DIRECTION_DOWN_LEFT:
+
+                break;
+            case DIRECTION_DOWN_RIGHT:
+
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
